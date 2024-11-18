b7a6dae89f9c29ccc46b37df9c41ddec82c7687a
Zainab-Junaid <153438938+Zainab-Junaid@users.noreply.github.com>
2024-11-19 00:13:23 +0500

    Update 1005B-Delete from the Left.py

--- 1005B-Delete from the Left.py
+++ 1005B-Delete from the Left.py
@@ -1,1 +1,31 @@
-
+s1 = input()
+s2 = input()
+z1 = s1[::-1]
+z2 = s2[::-1]
+summ = len(s1)+len(s2)
+a = max(len(s1),len(s2))
+count = 0
+if a == len(s1):
+    for i in range(len(z2)):
+        if z1[i]== z2[i]:
+            count += 1
+        else:
+            break
+    if count >0:
+        d = abs((count +count)-summ)
+        print(d)
+    else:
+        print(summ)
+else:
+    for i in range(len(z1)):
+        if z1[i]== z2[i]:
+            count += 1
+        else:
+            break
+    if count >0:
+        d = abs((count +count)-summ)
+        print(d)
+    else:
+        print(summ)
+        
+        
