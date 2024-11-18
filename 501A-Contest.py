3701b8d84826bf2c9a5d2391e3ad53c92a69ae30
Zainab-Junaid <153438938+Zainab-Junaid@users.noreply.github.com>
2024-11-19 00:14:31 +0500

    Update 501A-Contest.py

--- 501A-Contest.py
+++ 501A-Contest.py
@@ -1,1 +1,14 @@
+a,b,c,d = map(int,input().split())
+m1 = (a*3)/10
+m2 = a - ((a/250)*c)
+misha = max(m1,m2)
+v1 = (b*3)/10
+v2 = b - ((b/250)*d)
+vasya = max(v1,v2)
 
+if misha < vasya:
+    print("Vasya")
+elif misha > vasya:
+    print("Misha")
+else:
+    print("Tie")
