cf50c1d996f9432084df8fb7acbc756b642c6648
Zainab-Junaid <153438938+Zainab-Junaid@users.noreply.github.com>
2024-11-18 23:49:47 +0500

    Update 149A-Business trip.py

--- 149A-Business trip.py
+++ 149A-Business trip.py
@@ -1,1 +1,20 @@
-
+n = int(input())
+l = list(map(int,input().split()))
+l.sort()
+if n == 0 :
+    print(0)
+elif n in l:
+    print(1)
+else:
+    summ = 0
+    count = 0
+    for i in range(len(l)-1,-1,-1):
+        summ += l[i]
+        count += 1
+        if summ >= n:
+            print(count)
+            break
+    else:
+        print(-1)
+    
+         
