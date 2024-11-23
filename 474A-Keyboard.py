adbd1be352daa21d68b8931beea2bdb458979e14
Zainab Junaid <153438938+Zainab-Junaid@users.noreply.github.com>
2024-11-23 19:57:16 +0500

    Update 474A-Keyboard.py

--- 474A-Keyboard.py
+++ 474A-Keyboard.py
@@ -1,1 +1,17 @@
-
+h = input()
+s = input()
+l = 'qwertyuiopasdfghjkl;zxcvbnm,./'
+if h == 'R':
+    for i in range(len(s)):
+        if s[i] == 'q':
+            print('/',end='')
+        else:
+            ind = l.index(s[i])
+            print(l[ind-1],end='')
+else:
+    for i in range(len(s)):
+        if s[i] == '/':
+            print('q',end='')
+        else:
+            ind = l.index(s[i])
+            print(l[ind+1],end='')
