d94050270e4660b7cfaafe6909c37f7be779d377
Zainab Junaid <153438938+Zainab-Junaid@users.noreply.github.com>
2024-11-23 19:54:01 +0500

    Update 1840A-Cipher Shifer.py

--- 1840A-Cipher Shifer.py
+++ 1840A-Cipher Shifer.py
@@ -1,1 +1,15 @@
-
+t = int(input())
+for  _ in range(t):
+    n = int(input())
+    s = input()
+    a = 0
+    while a < n:
+        for i in range(a+1,n):
+            if s[a] == s[i]:
+                print(s[a],end='')
+                a = i+1
+                break
+    print()
+            
+   
+        
