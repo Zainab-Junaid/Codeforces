1191e39c09dcdfd1add420de916d513fbbefa9ae
Zainab Junaid <153438938+Zainab-Junaid@users.noreply.github.com>
2024-11-24 17:34:22 +0500

    Update 69A-Young Physicist.py

--- 69A-Young Physicist.py
+++ 69A-Young Physicist.py
@@ -1,1 +1,11 @@
-
+t = int(input())
+sumx = sumy = sumz = 0
+for _ in range(t):
+    x,y,z = map(int,input().split())
+    sumx += x
+    sumy += y
+    sumz += z
+if sumx == 0 and sumy == 0 and sumz == 0:
+    print('YES')
+else:
+    print('NO')
