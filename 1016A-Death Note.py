c8bed3f9ff9f85ff54d5c3525805733cd7c1da83
Zainab Junaid <153438938+Zainab-Junaid@users.noreply.github.com>
2024-11-24 17:27:04 +0500

    Update 1016A-Death Note.py

--- 1016A-Death Note.py
+++ 1016A-Death Note.py
@@ -1,1 +1,16 @@
-
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+extra = 0   
+for i in range(n):
+    count = 0      # for page no
+    if extra > a[i]:
+        extra -= a[i]
+        a[i] = 0
+    elif extra > 0:
+        count += 1
+        a[i] -= extra
+        extra = 0
+    if a[i]:
+        count += a[i] // m
+        extra = m - a[i] % m
+    print(count)
