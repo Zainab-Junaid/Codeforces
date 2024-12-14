3c9a79bfc737076ee4ff49683598aae0a486fd3f
Zainab Junaid <153438938+Zainab-Junaid@users.noreply.github.com>
2024-12-14 23:35:41 +0500

    Update 112A-Petya and Strings.py

--- 112A-Petya and Strings.py
+++ 112A-Petya and Strings.py
@@ -1,1 +1,11 @@
+def string(s1,s2):
+    if s1 == s2:
+        return 0
+    elif s1 > s2:
+        return 1
+    return -1
 
+s1 = input().lower()
+s2 = input().lower()
+result = string(s1,s2)
+print(result)
