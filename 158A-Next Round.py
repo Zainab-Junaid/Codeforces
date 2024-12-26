3b6dc2c4274ede40e2092eb949be82fddbe3f7c3
Zainab Junaid <153438938+Zainab-Junaid@users.noreply.github.com>
2024-12-27 03:11:35 +0500

    Update 158A-Next Round.py

--- 158A-Next Round.py
+++ 158A-Next Round.py
@@ -1,1 +1,13 @@
+def round(l,k):
+    count = 0
+    if set(l) == {0}:
+        return 0
+    for i in range(len(l)):
+        if l[i] >= l[k-1] and l[i] != 0:
+            count+= 1
+    return count
 
+n,k = map(int,input().split())
+l = list(map(int,input().split()))
+result = round(l,k)
+print(result)
