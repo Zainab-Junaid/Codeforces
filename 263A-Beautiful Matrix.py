0771b08967dd06c780ba88b81d6ef36d65a725e6
Zainab Junaid <153438938+Zainab-Junaid@users.noreply.github.com>
2025-01-01 01:17:55 +0500

    Update 263A-Beautiful Matrix.py

--- 263A-Beautiful Matrix.py
+++ 263A-Beautiful Matrix.py
@@ -1,1 +1,11 @@
-
+d = []
+for _ in range(5):
+    l = list(map(int,input().split()))
+    d.append(l)
+for i in range(5):
+    for j in range(5):
+        if d[i][j] == 1:
+            row = i
+            colm = j
+result = abs((row+1)-3) + abs((colm+1)-3)
+print(result)
