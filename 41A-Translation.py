d8fbef37fd054ab065e26fcc6df2eaec95ccee3c
Zainab Junaid <153438938+Zainab-Junaid@users.noreply.github.com>
2025-01-01 23:07:03 +0500

    Update 41A-Translation.py

--- 41A-Translation.py
+++ 41A-Translation.py
@@ -1,1 +1,11 @@
-
+def trans(s,t):
+    t1=''
+    for i in range(len(s)-1,-1,-1):
+        t1+= s[i]
+    if t1 == t:
+        return "YES"
+    return "NO"
+s = input()
+t = input()  
+result = trans(s,t)
+print(result)
