7a009022d9d18212f4d2838084aa21d15ecfcb53
Zainab Junaid <153438938+Zainab-Junaid@users.noreply.github.com>
2025-01-01 23:08:22 +0500

    Update 677A-Vanya and Fence.py

--- 677A-Vanya and Fence.py
+++ 677A-Vanya and Fence.py
@@ -1,1 +1,9 @@
-
+n,h = map(int,input().split())
+a = list(map(int,input().split()))
+count = 0
+for i in range(len(a)):
+    if a[i] <= h:
+        count+= 1
+    else:
+        count+= 2
+print(count)
